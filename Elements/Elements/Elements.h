1be36bc69003af01c70f0dbb423d5b2a11aca7b1
Joachim Kret <joachimkret@macbook.pro>
2021-01-07 15:31:53 +0100

    Added base project.

---
+++ Elements/Elements/Elements.h
@@ -0,0 +1,18 @@
+//
+//  Elements.h
+//  Elements
+//
+//  Created by Joachim Kret on 07/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Elements.
+FOUNDATION_EXPORT double ElementsVersionNumber;
+
+//! Project version string for Elements.
+FOUNDATION_EXPORT const unsigned char ElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elements/PublicHeader.h>
+
+
