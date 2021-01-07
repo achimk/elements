b53ccf2d40f1ddc64cef38217a99a19276e3cb5b
Joachim Kret <joachimkret@macbook.pro>
2021-01-07 16:18:15 +0100

    Extensions updates.

---
+++ Elements/Elements/Core/Extensions/String+Extensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+import CommonCrypto
+
+extension String {
+
+    // source: https://stackoverflow.com/a/55356729
+    var md5: String {
+        let data = Data(self.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+            return hash
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
+
